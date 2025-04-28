0c837e5bd533fb552b6be4c12f2bd5f3b7005e7c
Niklas Röttger <niklas.roettger@outlook.de>
2025-04-28 18:08:56 +0200

    extend dummy data

--- src/data.ts
+++ src/data.ts
@@ -71,4 +71,64 @@ export const events = [
       },
     ],
   },
+  {
+    id: 2,
+    name: "Heranwachsen 2",
+    start: "2025-05-02T10:00:00",
+    end: "2025-05-07T20:00:00",
+    preparation: "Eier, aus denen nichts Spannendes schlüpfen kann ausbrüten.",
+    specials: [1, 2, 3],
+    focus: [
+      {
+        pokemonName: "Riolu",
+        reasons: [1, 2],
+      },
+      {
+        pokemonName: "Karpador",
+        reasons: [4, 5, 2, 6],
+      },
+      {
+        pokemonName: "Wablu",
+        reasons: [4, 5],
+      },
+      {
+        pokemonName: "Meikro",
+        reasons: [4, 3],
+      },
+      {
+        pokemonName: "Toxel",
+        reasons: [1],
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Heranwachsen 3",
+    start: "2025-04-02T10:00:00",
+    end: "2025-04-07T20:00:00",
+    preparation: "Eier, aus denen nichts Spannendes schlüpfen kann ausbrüten.",
+    specials: [1, 2, 3],
+    focus: [
+      {
+        pokemonName: "Riolu",
+        reasons: [1, 2],
+      },
+      {
+        pokemonName: "Karpador",
+        reasons: [4, 5, 2, 6],
+      },
+      {
+        pokemonName: "Wablu",
+        reasons: [4, 5],
+      },
+      {
+        pokemonName: "Meikro",
+        reasons: [4, 3],
+      },
+      {
+        pokemonName: "Toxel",
+        reasons: [1],
+      },
+    ],
+  },
 ];
