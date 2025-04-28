d712d479d09634de8cf7de0804e360bdba503685
Niklas Röttger <niklas.roettger@outlook.de>
2025-04-28 17:42:46 +0200

    adjust header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import ThemeToggle from "@/components/ThemeToggle";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  max-width: 500px;
+  margin: 1rem auto;
+`;
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <h1>Pokémon Go Events</h1>
       <ThemeToggle />
-    </header>
+    </StyledHeader>
   );
 }
