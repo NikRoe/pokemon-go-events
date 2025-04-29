aacdf8a460139dd31ab4547b6c40b3c83acbd641
Niklas Röttger <niklas.roettger@outlook.de>
2025-04-29 09:30:19 +0200

    add theme to localstorage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "@/styles/themes/darkTheme";
 import { lightTheme } from "@/styles/themes/lightTheme";
+import useLocalStorageState from "use-local-storage-state";
 
 type ThemeMode = "light" | "dark";
 
@@ -25,7 +26,9 @@ interface ThemeProviderProps {
 }
 
 export function CustomThemeProvider({ children }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<ThemeMode>("dark");
+  const [theme, setTheme] = useLocalStorageState<ThemeMode>("theme", {
+    defaultValue: "dark",
+  });
 
   function toggleTheme() {
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
